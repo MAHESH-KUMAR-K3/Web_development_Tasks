bec3172ec2dc868f81148b634867501bdf12f0b7
MAHESH-KUMAR-K3 <165408103+MAHESH-KUMAR-K3@users.noreply.github.com>
2024-12-24 16:55:32 +0530

    Add files via upload

---
+++ ASSIGNMENTS/JAVA SCRIPT/12-12-2024/script.js
@@ -0,0 +1,20 @@
+
+let u_name = prompt("Enter your name");
+let prod = prompt("Enter your product");
+let cost = prompt("Enter the cost");
+let date = prompt("Enter the day of purchase");
+let festie = prompt("Enter the occasion");
+let shopping = prompt("Enter platform");
+
+let obj =
+{
+    username:u_name,
+    product:prod,
+    cost:cost,
+    day:date,
+    occasion:festie,
+    platfor:shopping,
+}
+
+console.log(`${obj.username} has purchased a ${obj.product} which costs ${obj.cost} on ${obj.day} on occassion of ${obj.occasion} in ${obj.platfor}`)
+
---
+++ ASSIGNMENTS/JAVA SCRIPT/20-12-2024/script.js
@@ -0,0 +1,19 @@
+// 1.what is the result of the 10+'5'
+console.log(10+"5")
+// output: 105
+console.log(10-"5")// output: 105
+console.log(10*"5")// output: 50
+console.log(10/"5")// output: 2
+console.log(10%"5")// output: 0
+
+
+
+
+// 2.What is the value of x after x = 10; x += 5;?
+// ...............................................
+let x=10;
+x+=5;
+console.log(x)// output: 15
+
+
+// 3.What is the result of 10 - null?
